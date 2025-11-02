73f5ec30a424598ca1988d3999da22613fe18969
PhanTrung123 <phanquangtrung30042004@gmail.com>
2025-11-02 18:00:28 +0700

    upload

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
-// server.js
 const express = require("express");
 const cors = require("cors");
 const siteData = require("../frontend/data/siteData");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/api/landing", (req, res) => {
   res.json(siteData);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
