5b0ecdc82873dec0433af7b4851c4be1936f8133
PhanTrung123 <phanquangtrung30042004@gmail.com>
2025-11-10 10:48:45 +0700

    upload

--- frontend/src/page/Cart1.jsx
+++ frontend/src/page/Cart1.jsx
@@ -38,7 +38,7 @@ const Cart1 = () => {
       </header>
       <div className="w-full  my-10">
         <div className=" rounded-[15px]  mx-auto  xl:flex max-w-[1320px] justify-between gap-[55px]">
-          <div className="py-[25px] px-[25px] lg:py-[50px]  lg:px-[45px] bg-white w-full  ">
+          <div className="max-lg:py-[25px] max-lg:px-[25px] py-[50px]  px-[45px] bg-white w-full  ">
             <div className="flex items-center">
               <img src="/icons/arrow.png" alt="arrow" />
               <h3 className="font-semibold text-[18px] leading-[100%] text-[#1E1E1E]">
@@ -60,21 +60,21 @@ const Cart1 = () => {
               <div className="w-full flex items-center  max-h-[100px] rounded-[15px] shadow mt-[29px]">
                 <div
                   key={item.id}
-                  className="w-full flex md:flex-1 items-center px-[10px] py-[9px] pl-[9px] pr-0 lg:pr-[23px] "
+                  className="w-full flex md:flex-1 items-center px-[10px] py-[9px] pl-[9px] max-lg:pr-0 pr-[23px] "
                 >
                   <img
                     src={item.img}
                     className="w-[80px] h-auto aspect-square object-cover rounded-[8px]"
                   />
                   <div className="pl-[16px] lg:px-[18px]">
-                    <h3 className=" text-[rgba(30,30,30,1)] font-medium text-[16px] lg:text-[18px] leading-[100%]">
+                    <h3 className=" text-[rgba(30,30,30,1)] font-medium max-lg:text-[16px] text-[18px] leading-[100%]">
                       {item.name}
                     </h3>
-                    <p className=" mt-[3px] font-medium text-[12px] lg:text-[14px] leading-[100%] flex-nowrap">
+                    <p className=" mt-[3px] font-medium text-[14px] max-lg:text-[12px] leading-[100%] flex-nowrap">
                       {item.desc}
                     </p>
-                    <div className=" items-center ml-auto flex md:hidden mt-1  gap-[6px]">
-                      <span className="font-semibold text-[18px] lg:text-[22px] lining-nums proportional-nums ">
+                    <div className=" items-center ml-auto max-md:flex hidden mt-1 gap-[6px]">
+                      <span className="font-semibold max-lg:text-[18px] text-[22px] lining-nums proportional-nums ">
                         {item.qty}
                       </span>
                       <img src="/icons/Scale.png" />
@@ -82,7 +82,7 @@ const Cart1 = () => {
                   </div>
                 </div>
                 <div className="flex flex-1 justify-between items-center mr-[23px]">
-                  <div className="hidden md:flex items-center ml-auto  gap-[6px]">
+                  <div className="max-md:hidden flex items-center ml-auto  gap-[6px]">
                     <span className="font-semibold text-[22px] lining-nums proportional-nums ">
                       {item.qty}
                     </span>
@@ -91,15 +91,15 @@ const Cart1 = () => {
                   <span className="font-medium ml-auto text-[14px] leading-[100%] ">
                     ${item.price}
                   </span>
-                  <button className="w-[20px] h-[20px] md:w-[25px] md:h-[25px] ml-[20px] lg:ml-[38px] items-center flex cursor-pointer  ">
+                  <button className="max-md:w-[20px] max-md:h-[20px] w-[25px] h-[25px] max-lg:ml-[20px] ml-[38px] items-center flex cursor-pointer  ">
                     <img src="/icons/trash.png" className="" />
                   </button>
                 </div>
               </div>
             ))}
           </div>
-          <div className="w-[90%] xl:w-[388px] mx-auto rounded-[20px] h-auto bg-[rgba(86,90,187,1)] my-[50px]">
-            <div className="flex flex-col  py-[21px] xl:py-0 lg:mt-[27px] mb-[21px] mx-[19px]">
+          <div className="max-xl:max-w-[90%] max-w-[388px] mx-auto rounded-[20px] h-auto bg-[rgba(86,90,187,1)] my-[50px]">
+            <div className="flex flex-col  max-xl:py-[21px] py-0 lg:mt-[27px] mb-[21px] mx-[19px]">
               <div className="flex items-center justify-between">
                 <h3 className="text-[rgba(254,252,252,1)] font-semibold text-[22px] leading-[100%]">
                   Card Details
@@ -110,10 +110,10 @@ const Cart1 = () => {
                 />
               </div>
               <div className="mt-[15px]">
-                <span className="text-[rgba(254,252,252,1)] font-medium size-[16px] leading-[100%]">
+                <span className="text-[rgba(254,252,252,1)] font-medium text-[16px] leading-[100%]">
                   Card type
                 </span>
-                <div className="w-full flex-wrap lg:flex-nowrap gap-[17px] flex flex-row mt-[14px] mx-auto justify-center ">
+                <div className="w-full max-lg:flex-wrap flex-nowrap gap-[17px] flex flex-row mt-[14px] mx-auto justify-center ">
                   <div className="">
                     <div className="w-[75px] h-[55px] flex items-center bg-[rgba(217,217,217,0.2)] rounded-[5px]">
                       <img src="/image/Icon_bank_01.png" />
@@ -218,11 +218,11 @@ const Cart1 = () => {
         </div>
       </div>
       <footer className="w-full border-t border-b border-[#e9eaec] bg-white">
-        <div className="max-w-[1320px] w-full mx-auto px-4 xl:px-0">
+        <div className="max-w-[1320px] w-full mx-auto max-xl:px-4 px-0">
           <Footer />
         </div>
-        <section className="flex-1 border-t border-[#e9eaec] w-full mx-auto px-4 sm:px-0">
-          <div className="max-w-[1320px] w-full mx-auto px-4 xl:px-0">
+        <section className="flex-1 border-t border-[#e9eaec] w-full mx-auto max-sm:px-4 px-0">
+          <div className="max-w-[1320px] w-full mx-auto max-xl:px-4 px-0">
             <Copyright />
           </div>
         </section>
